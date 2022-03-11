607080afaf8a54f9f4c4c34da810bcd3bd6deeb3
Paul Deen <paul.deen@dutchworkz.nl>
2022-03-11 17:12:19 +0100

    Remove ExpressionFramework.CodeGeneration solution, and use t4plus dotnet tool to perform code generation instead

---
+++ src/CodeGeneration/CodeGeneration.cmd
@@ -0,0 +1,8 @@
+dotnet new tool-manifest --force
+dotnet tool install pauldeen79.TextTemplateTransformationFramework.T4.Plus.Cmd --version 0.1.19
+dotnet t4plus run -f CodeGeneration.template BasePath:../ $T4.TempPath:./ GenerateMultipleFiles:True DryRun:False
+dotnet tool uninstall pauldeen79.TextTemplateTransformationFramework.T4.Plus.Cmd
+
+rem dotnet tool install --global pauldeen79.TextTemplateTransformationFramework.T4.Plus.Cmd --version 0.1.19
+rem t4plus run -f CodeGeneration.template BasePath:../ $T4.TempPath:./ GenerateMultipleFiles:True DryRun:False
+rem dotnet tool uninstall --global pauldeen79.TextTemplateTransformationFramework.T4.Plus.Cmd
