94717c85eb6aa46e3f3f05e6c7771a8baee08b2e
Paul Deen <paul.deen@dutchworkz.nl>
2022-03-11 21:01:36 +0100

    Attempt to fix build pipeline

--- src/CodeGeneration/CodeGeneration.cmd
+++ src/CodeGeneration/CodeGeneration.cmd
@@ -1,6 +1,7 @@
+cd ../../
 dotnet new tool-manifest --force
 dotnet tool install pauldeen79.TextTemplateTransformationFramework.T4.Plus.Cmd --version 0.1.19
-dotnet t4plus run -f CodeGeneration.template BasePath:../ GenerateMultipleFiles:True DryRun:False
+dotnet t4plus run -f src/CodeGeneration/CodeGeneration.template BasePath:src/ GenerateMultipleFiles:True DryRun:False
 dotnet tool uninstall pauldeen79.TextTemplateTransformationFramework.T4.Plus.Cmd
 
 rem dotnet tool install --global pauldeen79.TextTemplateTransformationFramework.T4.Plus.Cmd --version 0.1.19
