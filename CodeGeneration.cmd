6b5c303a06547124ae3acfbd7c3cab9a0f2af72c
Paul Deen <paul.deen@dutchworkz.nl>
2022-03-11 21:08:38 +0100

    Removed commented code in command file

--- CodeGeneration.cmd
+++ CodeGeneration.cmd
@@ -2,7 +2,3 @@ dotnet new tool-manifest --force
 dotnet tool install pauldeen79.TextTemplateTransformationFramework.T4.Plus.Cmd --version 0.1.19
 dotnet t4plus run -f src/CodeGeneration/CodeGeneration.template BasePath:src/ GenerateMultipleFiles:True DryRun:False
 dotnet tool uninstall pauldeen79.TextTemplateTransformationFramework.T4.Plus.Cmd
-
-rem dotnet tool install --global pauldeen79.TextTemplateTransformationFramework.T4.Plus.Cmd --version 0.1.19
-rem t4plus run -f CodeGeneration.template BasePath:../ GenerateMultipleFiles:True DryRun:False
-rem dotnet tool uninstall --global pauldeen79.TextTemplateTransformationFramework.T4.Plus.Cmd
