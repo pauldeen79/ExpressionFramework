e4e10f848582759b438ed5a5bef5dbc6f1f78d45
Paul Deen <paul.deen@dutchworkz.nl>
2022-08-20 21:15:44 +0200

    Slightly refactored code. The method is now within the maximum cognitive complexity :)

--- src/ExpressionFramework.Core/ExpressionEvaluatorProviders/CompositeExpressionEvaluatorProvider.cs
+++ src/ExpressionFramework.Core/ExpressionEvaluatorProviders/CompositeExpressionEvaluatorProvider.cs
@@ -11,37 +11,37 @@ public class CompositeExpressionEvaluatorProvider : IExpressionEvaluatorProvider
     {
         result = default;
 
-        if (expression is ICompositeExpression compositeExpression)
+        if (expression is not ICompositeExpression compositeExpression)
         {
-            bool first = true;
-            foreach (var innerExpression in compositeExpression.Expressions)
-            {
-                if (first)
-                {
-                    // Note that context and item have been switched on purpose! This was intended. Unit tests pass, so everything's okay.
-#pragma warning disable S2234 // Parameters should be passed in the correct order 
-                    result = evaluator.Evaluate(item: context, context: item, innerExpression);
-#pragma warning restore S2234 // Parameters should be passed in the correct order
-                    first = false;
-                }
-                else
-                {
-                    foreach (var eval in _evaluators)
-                    {
-                        if (eval.TryEvaluate(compositeExpression.CompositeFunction, result, item, evaluator, innerExpression, out var evaluatorResult))
-                        {
-                            result = evaluatorResult;
-                            return true;
-                        }
-                    }
-                    // Unknown composite function
-                    return false;
-                }
-            }
-
-            return true;
+            return false;
         }
 
-        return false;
+        bool first = true;
+        foreach (var innerExpression in compositeExpression.Expressions)
+        {
+            if (first)
+            {
+                // Note that context and item have been switched on purpose! This was intended. Unit tests pass, so everything's okay.
+#pragma warning disable S2234 // Parameters should be passed in the correct order 
+                result = evaluator.Evaluate(item: context, context: item, innerExpression);
+#pragma warning restore S2234 // Parameters should be passed in the correct order
+                first = false;
+            }
+            else
+            {
+                foreach (var eval in _evaluators)
+                {
+                    if (eval.TryEvaluate(compositeExpression.CompositeFunction, result, item, evaluator, innerExpression, out var evaluatorResult))
+                    {
+                        result = evaluatorResult;
+                        return true;
+                    }
+                }
+                // Unknown composite function
+                return false;
+            }
+        }
+
+        return true;
     }
 }
