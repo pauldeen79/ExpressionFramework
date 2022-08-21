b57d83cbcc892be686ccca84ee749ae055d989d0
Paul Deen <paul.deen@dutchworkz.nl>
2022-08-21 13:57:21 +0200

    Added missing unit tests

---
+++ src/ExpressionFramework.Core.Tests/ExpressionEvaluatorProviders/ContextExpressionEvaluatorProviderTests.cs
@@ -0,0 +1,36 @@
+﻿namespace ExpressionFramework.Core.Tests.ExpressionEvaluatorProviders;
+
+public class ContextExpressionEvaluatorProviderTests
+{
+    [Fact]
+    public void TryEvaluate_Returns_False_When_Expression_Is_Not_A_ContextExpression()
+    {
+        // Arrange
+        var sut = new ContextExpressionEvaluatorProvider();
+        var expressionMock = new Mock<IExpression>();
+        var expressionEvaluatorMock = new Mock<IExpressionEvaluator>();
+
+        // Act
+        var actual = sut.TryEvaluate(default, default, expressionMock.Object, expressionEvaluatorMock.Object, out var result);
+
+        // Assert
+        actual.Should().BeFalse();
+        result.Should().BeNull();
+    }
+
+    [Fact]
+    public void TryEvaluate_Returns_True_When_Expression_Is_A_ContextExpression()
+    {
+        // Arrange
+        var sut = new ContextExpressionEvaluatorProvider();
+        var expressionMock = new Mock<IContextExpression>();
+        var expressionEvaluatorMock = new Mock<IExpressionEvaluator>();
+
+        // Act
+        var actual = sut.TryEvaluate(default, 12345, expressionMock.Object, expressionEvaluatorMock.Object, out var result);
+
+        // Assert
+        actual.Should().BeTrue();
+        result.Should().Be(12345);
+    }
+}
---
+++ src/ExpressionFramework.Core.Tests/ExpressionEvaluatorProviders/ItemExpressionEvaluatorProviderTests.cs
@@ -0,0 +1,36 @@
+﻿namespace ExpressionFramework.Core.Tests.ExpressionEvaluatorProviders;
+
+public class ItemExpressionEvaluatorProviderTests
+{
+    [Fact]
+    public void TryEvaluate_Returns_False_When_Expression_Is_Not_A_ItemExpression()
+    {
+        // Arrange
+        var sut = new ItemExpressionEvaluatorProvider();
+        var expressionMock = new Mock<IExpression>();
+        var expressionEvaluatorMock = new Mock<IExpressionEvaluator>();
+
+        // Act
+        var actual = sut.TryEvaluate(default, default, expressionMock.Object, expressionEvaluatorMock.Object, out var result);
+
+        // Assert
+        actual.Should().BeFalse();
+        result.Should().BeNull();
+    }
+
+    [Fact]
+    public void TryEvaluate_Returns_True_When_Expression_Is_A_ItemExpression()
+    {
+        // Arrange
+        var sut = new ItemExpressionEvaluatorProvider();
+        var expressionMock = new Mock<IItemExpression>();
+        var expressionEvaluatorMock = new Mock<IExpressionEvaluator>();
+
+        // Act
+        var actual = sut.TryEvaluate(12345, null, expressionMock.Object, expressionEvaluatorMock.Object, out var result);
+
+        // Assert
+        actual.Should().BeTrue();
+        result.Should().Be(12345);
+    }
+}
