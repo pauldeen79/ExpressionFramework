c12c7fe64657787b9853b6475ba5b315cacdab4f
Paul Deen <paul.deen@dutchworkz.nl>
2022-08-22 07:22:33 +0200

    Added TODO

--- src/ExpressionFramework.Core/ExpressionEvaluatorProviders/ConditionalExpressionConditionResultExpressionProvider.cs
+++ src/ExpressionFramework.Core/ExpressionEvaluatorProviders/ConditionalExpressionConditionResultExpressionProvider.cs
@@ -1,5 +1,6 @@
 ﻿namespace ExpressionFramework.Core.ExpressionEvaluatorProviders;
 
+//TODO: Remove this class, and the interface and implementations of the domain model as well
 public class ConditionalExpressionConditionResultExpressionProvider : IExpressionEvaluatorProvider
 {
     public bool TryEvaluate(object? item, object? context, IExpression expression, IExpressionEvaluator evaluator, out object? result)
