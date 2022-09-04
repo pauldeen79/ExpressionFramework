07aabbb82b91a8ea3756789525e110a0ef590b65
Paul Deen <paul.deen@dutchworkz.nl>
2022-09-04 12:24:33 +0200

    Added test project for vNext

---
+++ src/ExpressionFramework.Domain.Tests/ImplicitUsings.cs
@@ -0,0 +1,4 @@
+﻿global using System.ComponentModel.DataAnnotations;
+global using ExpressionFramework.Domain.Expressions;
+global using FluentAssertions;
+global using Xunit;
---
+++ src/ExpressionFramework.Domain.Tests/Unit/Expressions/FieldExpressionTests.cs
@@ -0,0 +1,13 @@
+﻿namespace ExpressionFramework.Domain.Tests.Unit.Expressions;
+
+public class FieldExpressionTests
+{
+    [Fact]
+    public void Should_Throw_On_Construction_With_Empty_FieldName()
+    {
+        // Act
+        this.Invoking(_ => new FieldExpression(string.Empty))
+            .Should().Throw<ValidationException>()
+            .WithMessage("The FieldName field is required.");
+    }
+}
