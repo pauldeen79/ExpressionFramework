33743db4dde893609db502820d32cb619bbd3297
Paul Deen <paul.deen@dutchworkz.nl>
2022-09-04 12:58:48 +0200

    Minor: Removed unnecessary validation in operators, as there are no properties. This is purely a marker class.

--- src/CodeGenerationNext/CodeGenerationProviders/OverrideOperatorEntities.cs
+++ src/CodeGenerationNext/CodeGenerationProviders/OverrideOperatorEntities.cs
@@ -9,6 +9,7 @@ public class OverrideOperatorEntities : ExpressionFrameworkCSharpClassBase
     protected override bool EnableEntityInheritance => true;
     protected override bool EnableBuilderInhericance => true;
     protected override IClass? BaseClass => CreateBaseclass(typeof(IOperator), "ExpressionFramework.Domain");
+    protected override bool ValidateArgumentsInConstructor => false; // there are no properties in operators, so this is not necessary
 
     public override object CreateModel()
         => GetImmutableClasses(GetOverrideOperatorModels(), "ExpressionFramework.Domain.Operators")
