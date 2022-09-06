3a4dc0ed2f4d0e0000817cd7e939f9d63cbfe85b
Paul Deen <paul.deen@dutchworkz.nl>
2022-09-06 08:00:29 +0200

    Added missing required attributes to model

--- src/CodeGenerationNext/Models/ICase.cs
+++ src/CodeGenerationNext/Models/ICase.cs
@@ -2,6 +2,8 @@
 
 public interface ICase
 {
+    [Required]
     IReadOnlyCollection<ICondition> Conditions { get; }
+    [Required]
     IExpression Expression { get; }
 }
--- src/CodeGenerationNext/Models/ICondition.cs
+++ src/CodeGenerationNext/Models/ICondition.cs
@@ -2,8 +2,11 @@
 
 public interface ICondition
 {
+    [Required]
     IExpression LeftExpression { get; }
+    [Required]
     IOperator Operator { get; }
+    [Required]
     IExpression RightExpression { get; }
     bool StartGroup { get; }
     bool EndGroup { get; }
