7d573f3cd47fd8195e1598cb076360c4a74efc81
Paul Deen <paul.deen@dutchworkz.nl>
2022-09-07 20:45:16 +0200

    Fixed sonar issue

--- src/ExpressionFramework.Domain/OperatorHandlers/EqualsOperatorHandler.cs
+++ src/ExpressionFramework.Domain/OperatorHandlers/EqualsOperatorHandler.cs
@@ -3,7 +3,24 @@
 public class EqualsOperatorHandler : OperatorHandlerBase<EqualsOperator>
 {
     protected override bool Evaluate(object? leftValue, object? rightValue)
-        => (leftValue == null && rightValue == null)
-        || (leftValue is string leftString && rightValue is string rightString && leftString.Equals(rightString, StringComparison.OrdinalIgnoreCase))
-        || (leftValue != null && rightValue != null && leftValue.Equals(rightValue));
+    {
+        if (leftValue == null && rightValue == null)
+        {
+            return true;
+        }
+
+        if (leftValue == null || rightValue == null)
+        {
+            return false;
+        }
+
+        if (leftValue is string leftString
+            && rightValue is string rightString
+            && leftString.Equals(rightString, StringComparison.OrdinalIgnoreCase))
+        {
+            return true;
+        }
+
+        return leftValue.Equals(rightValue);
+    }
 }
