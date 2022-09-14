90bd77207de6a981b61c945a4e4db1cae5fbd431
Paul Deen <paul.deen@dutchworkz.nl>
2022-09-14 21:09:25 +0200

    Added some missing unit tests

---
+++ src/ExpressionFramework.Domain.Tests/Unit/Expressions/OperatorExpressionTests.cs
@@ -0,0 +1,32 @@
+﻿namespace ExpressionFramework.Domain.Tests.Unit.Expressions;
+
+public class OperatorExpressionTests
+{
+    [Fact]
+    public void Evaluate_Returns_Error_When_Operator_Evaluation_Fails()
+    {
+        // Arrange
+        var sut = new OperatorExpression(new ErrorExpression("Kaboom"), new EqualsOperator(), new EmptyExpression());
+
+        // Act
+        var actual = sut.Evaluate(null);
+
+        // Assert
+        actual.Status.Should().Be(ResultStatus.Error);
+        actual.ErrorMessage.Should().Be("Kaboom");
+    }
+
+    [Fact]
+    public void Evaluate_Returns_Result_When_Operator_Evaluation_Succeeds()
+    {
+        // Arrange
+        var sut = new OperatorExpression(new ConstantExpression("1"), new NotEqualsOperator(), new ConstantExpression("2"));
+
+        // Act
+        var actual = sut.Evaluate(null);
+
+        // Assert
+        actual.Status.Should().Be(ResultStatus.Ok);
+        actual.GetValueOrThrow().Should().BeEquivalentTo(true);
+    }
+}
---
+++ src/ExpressionFramework.Domain.Tests/Unit/OperatorTests.cs
@@ -0,0 +1,32 @@
+﻿namespace ExpressionFramework.Domain.Tests.Unit;
+
+public class OperatorTests
+{
+    [Fact]
+    public void Evaluate_Returns_Error_From_LeftExpression_When_Evaluation_Fails()
+    {
+        // Arrange
+        var sut = new EqualsOperator();
+
+        // Act
+        var actual = sut.Evaluate(null, new ErrorExpression("Kaboom"), new EmptyExpression());
+
+        // Assert
+        actual.Status.Should().Be(ResultStatus.Error);
+        actual.ErrorMessage.Should().Be("Kaboom");
+    }
+
+    [Fact]
+    public void Evaluate_Returns_Error_From_RightExpression_When_Evaluation_Fails()
+    {
+        // Arrange
+        var sut = new EqualsOperator();
+
+        // Act
+        var actual = sut.Evaluate(null, new EmptyExpression(), new ErrorExpression("Kaboom"));
+
+        // Assert
+        actual.Status.Should().Be(ResultStatus.Error);
+        actual.ErrorMessage.Should().Be("Kaboom");
+    }
+}
