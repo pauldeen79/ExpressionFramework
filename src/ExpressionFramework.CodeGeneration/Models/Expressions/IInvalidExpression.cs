1ac3992d242772b8b94c0f596657ed589409b05d
Paul Deen <paul.deen@dutchworkz.nl>
2022-09-17 10:59:47 +0200

    Added InvalidExpression, so you can return validation errors from expressions

---
+++ src/ExpressionFramework.CodeGeneration/Models/Expressions/IInvalidExpression.cs
@@ -0,0 +1,7 @@
+﻿namespace ExpressionFramework.CodeGeneration.Models.Expressions;
+
+public interface IInvalidExpression : IExpression
+{
+    string ErrorMessage { get; }
+    IReadOnlyCollection<ValidationError> ValidationErrors { get; }
+}
