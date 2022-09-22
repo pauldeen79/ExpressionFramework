3e197e9f628a4e1aa6b4c9fa73f593e7772a3bb6
Paul Deen <paul.deen@dutchworkz.nl>
2022-09-22 07:20:55 +0200

    Refactored ChainedExpression to use single LINQ statement instead of procedural code (foreach loop)

--- src/ExpressionFramework.Domain/Expressions/ChainedExpression.cs
+++ src/ExpressionFramework.Domain/Expressions/ChainedExpression.cs
@@ -4,25 +4,18 @@ public partial record ChainedExpression
 {
     public override Result<object?> Evaluate(object? context)
     {
-        Result<object?>? result = null;
-        var first = true;
-        foreach (var exp in Expressions)
+        if (!Expressions.Any())
         {
-            if (first)
-            {
-                result = exp.Evaluate(context);
-                first = false;
-            }
-            else
-            {
-                result = exp.Evaluate(result!.Value);
-            }
-            if (!result.IsSuccessful())
-            {
-                return result;
-            }
+            return Result<object?>.Invalid("No expressions found");
         }
 
-        return result ?? Result<object?>.Invalid("No expressions found");
+        return Expressions.Aggregate(Result<object?>.Success(context), (seed, accumulator) =>
+        {
+            if (!seed.IsSuccessful())
+            {
+                return seed;
+            }
+            return accumulator.Evaluate(seed.Value);
+        });
     }
 }
