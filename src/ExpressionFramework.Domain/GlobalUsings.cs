c1c42c09213a7eea5ef123d2236d59212612d10f
pauldeen79 <pauldeen@gmx.li>
2022-09-27 14:54:04 +0200

    Minor: Removed unused usings

--- src/ExpressionFramework.Domain/GlobalUsings.cs
+++ src/ExpressionFramework.Domain/GlobalUsings.cs
@@ -7,7 +7,6 @@ global using System.Reflection;
 global using System.Text;
 global using CrossCutting.Common.Extensions;
 global using CrossCutting.Common.Results;
-global using ExpressionFramework.Domain.Domains;
 global using ExpressionFramework.Domain.Attributes;
-global using ExpressionFramework.Domain.Expressions;
+global using ExpressionFramework.Domain.Domains;
 global using ExpressionFramework.Domain.Operators;
