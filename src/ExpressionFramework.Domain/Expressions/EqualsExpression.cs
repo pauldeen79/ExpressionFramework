92087a337b15c8fdf57eb6bbdeccc8fde577d16d
pauldeen79 <pauldeen@gmx.li>
2022-09-28 08:30:10 +0200

    Added descriptor provider attributes to EqualsExpression

--- src/ExpressionFramework.Domain/Expressions/EqualsExpression.cs
+++ src/ExpressionFramework.Domain/Expressions/EqualsExpression.cs
@@ -1,5 +1,11 @@
 ﻿namespace ExpressionFramework.Domain.Expressions;
 
+[ExpressionDescription("This expression evaluates two expressions, and compares the two results. It will return true when they are equal, or false otherwise.")]
+[ParameterDescription(nameof(FirstExpression), "First expression")]
+[ParameterRequired(nameof(FirstExpression), true)]
+[ParameterDescription(nameof(SecondExpression), "Second expression")]
+[ParameterRequired(nameof(SecondExpression), true)]
+[ReturnValue(ResultStatus.Ok, "true of false", "This result will always be returned")]
 public partial record EqualsExpression
 {
     public override Result<object?> Evaluate(object? context)
