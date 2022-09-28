94d90ca7d5f81072520f2246180553be870f1647
pauldeen79 <pauldeen@gmx.li>
2022-09-28 08:26:40 +0200

    Added descriptor provider attributes to EmptyExpression

--- src/ExpressionFramework.Domain.Tests/Unit/Expressions/EmptyExpressionTests.cs
+++ src/ExpressionFramework.Domain.Tests/Unit/Expressions/EmptyExpressionTests.cs
@@ -14,4 +14,21 @@ public class EmptyExpressionTests
         // Assert
         actual.Should().BeEmpty();
     }
+
+    [Fact]
+    public void Can_Determine_Descriptor_Provider()
+    {
+        // Arrange
+        var sut = new ReflectionExpressionDescriptorProvider(typeof(EmptyExpression));
+
+        // Act
+        var result = sut.Get();
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Name.Should().Be(nameof(EmptyExpression));
+        result.Parameters.Should().BeEmpty();
+        result.ReturnValues.Should().ContainSingle();
+        result.ContextIsRequired.Should().BeNull();
+    }
 }
--- src/ExpressionFramework.Domain/Expressions/EmptyExpression.cs
+++ src/ExpressionFramework.Domain/Expressions/EmptyExpression.cs
@@ -1,5 +1,7 @@
 ﻿namespace ExpressionFramework.Domain.Expressions;
 
+[ExpressionDescription("This expression always returns an empty value")]
+[ReturnValue(ResultStatus.Ok, "Empty", "This result will always be returned")]
 public partial record EmptyExpression
 {
     public override Result<object?> Evaluate(object? context)
