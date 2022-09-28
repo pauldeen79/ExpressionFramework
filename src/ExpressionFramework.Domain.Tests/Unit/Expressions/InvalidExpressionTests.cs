34b80770366f6182433e582586ef68c061b9481c
pauldeen79 <pauldeen@gmx.li>
2022-09-28 08:33:20 +0200

    Added descriptor provider attributes to InvalidExpression

---
+++ src/ExpressionFramework.Domain.Tests/Unit/Expressions/InvalidExpressionTests.cs
@@ -0,0 +1,21 @@
+﻿namespace ExpressionFramework.Domain.Tests.Unit.Expressions;
+
+public class InvalidExpressionTests
+{
+    [Fact]
+    public void Can_Determine_Descriptor_Provider()
+    {
+        // Arrange
+        var sut = new ReflectionExpressionDescriptorProvider(typeof(InvalidExpression));
+
+        // Act
+        var result = sut.Get();
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Name.Should().Be(nameof(InvalidExpression));
+        result.Parameters.Should().HaveCount(2);
+        result.ReturnValues.Should().ContainSingle();
+        result.ContextIsRequired.Should().BeNull();
+    }
+}
--- src/ExpressionFramework.Domain/Expressions/InvalidExpression.cs
+++ src/ExpressionFramework.Domain/Expressions/InvalidExpression.cs
@@ -1,5 +1,11 @@
 ﻿namespace ExpressionFramework.Domain.Expressions;
 
+[ExpressionDescription("Returns an invalid result")]
+[ParameterDescription(nameof(ErrorMessage), "Error message to use")]
+[ParameterRequired(nameof(ErrorMessage), true)]
+[ParameterDescription(nameof(ValidationErrors), "Validation errors to use")]
+[ParameterRequired(nameof(ValidationErrors), true)]
+[ReturnValue(ResultStatus.Invalid, "Empty", "This result will always be returned")]
 public partial record InvalidExpression
 {
     public override Result<object?> Evaluate(object? context)
