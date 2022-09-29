05a308805b93099fc18cc34ea0ffa07565b3e93b
Paul Deen <paul.deen@dutchworkz.nl>
2022-09-29 19:18:35 +0200

    Added descriptor provider attributes to IsSmallerOperator, IsSmallerOrEqualOperator, IsGreaterOperator and IsGreaterOrEqualOperator

---
+++ src/ExpressionFramework.Domain.Tests/Unit/Operators/IsGreaterOrEqualOperatorTests.cs
@@ -0,0 +1,24 @@
+﻿namespace ExpressionFramework.Domain.Tests.Unit.Operators;
+
+public class IsGreaterOrEqualOperatorTests
+{
+    [Fact]
+    public void Can_Determine_Descriptor_Provider()
+    {
+        // Arrange
+        var sut = new ReflectionOperatorDescriptorProvider(typeof(IsGreaterOrEqualOperator));
+
+        // Act
+        var result = sut.Get();
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Name.Should().Be(nameof(IsGreaterOrEqualOperator));
+        result.Parameters.Should().BeEmpty();
+        result.UsesLeftValue.Should().BeTrue();
+        result.LeftValueTypeName.Should().NotBeEmpty();
+        result.UsesRightValue.Should().BeTrue();
+        result.RightValueTypeName.Should().NotBeEmpty();
+        result.ReturnValues.Should().ContainSingle();
+    }
+}
