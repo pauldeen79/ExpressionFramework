ad524198c4f8a53c3f508600cfa38be7190c69df
Paul Deen <paul.deen@dutchworkz.nl>
2022-10-02 16:35:40 +0200

    Slightly improved Program.cs by extracting known generation typename suffixes

--- src/ExpressionFramework.CodeGeneration/Program.cs
+++ src/ExpressionFramework.CodeGeneration/Program.cs
@@ -16,9 +16,10 @@ internal static class Program
         var settings = new CodeGenerationSettings(basePath, generateMultipleFiles, false, dryRun);
 
         // Generate code
+        var generationTypeNames = new[] { "Entities", "Builders", "BuilderFactory" };
         var generators = typeof(ExpressionFrameworkCSharpClassBase).Assembly.GetExportedTypes().Where(x => x.BaseType == typeof(ExpressionFrameworkCSharpClassBase)).ToArray();
-        var generationTypes = generators.Where(x => x.Name.EndsWithAny("Entities", "Builders", "Factory"));
-        var scaffoldingTypes = generators.Where(x => !x.Name.EndsWithAny("Entities", "Builders", "Factory"));
+        var generationTypes = generators.Where(x => x.Name.EndsWithAny(generationTypeNames));
+        var scaffoldingTypes = generators.Where(x => !x.Name.EndsWithAny(generationTypeNames));
         _ = generationTypes.Select(x => (ExpressionFrameworkCSharpClassBase)Activator.CreateInstance(x)!).Select(x => GenerateCode.For(settings.ForGeneration(), multipleContentBuilder, x)).ToArray();
         _ = scaffoldingTypes.Select(x => (ExpressionFrameworkCSharpClassBase)Activator.CreateInstance(x)!).Select(x => GenerateCode.For(settings.ForScaffolding(), multipleContentBuilder, x)).ToArray();
 
