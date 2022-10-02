0f90e813b78be28dc6286afa5726f61c88496c08
Paul Deen <paul.deen@dutchworkz.nl>
2022-10-02 08:35:57 +0200

    Simplified and generatlized code generation

--- src/ExpressionFramework.CodeGeneration/CodeGenerationProviders/ExpressionFrameworkCSharpClassBase.cs
+++ src/ExpressionFramework.CodeGeneration/CodeGenerationProviders/ExpressionFrameworkCSharpClassBase.cs
@@ -10,20 +10,20 @@ public abstract partial class ExpressionFrameworkCSharpClassBase : CSharpClassBa
     protected override Type RecordCollectionType => typeof(IReadOnlyCollection<>);
     protected override Type RecordConcreteCollectionType => typeof(ReadOnlyValueCollection<>);
     protected override string FileNameSuffix => ".template.generated";
-    protected override string RootNamespace => "ExpressionFramework.Domain";
+    protected static string ProjectName => "ExpressionFramework";
+    protected override string RootNamespace => $"{ProjectName}.Domain";
+    protected static string CodeGenerationRootNamespace => $"{ProjectName}.CodeGeneration";
     protected override Type BuilderClassCollectionType => typeof(IEnumerable<>);
     protected override bool AddBackingFieldsForCollectionProperties => true;
     protected override bool AddPrivateSetters => true;
 
-    private static string CodeGenerationRootNamespace => "ExpressionFramework.CodeGeneration";
-
     protected override string GetFullBasePath()
-        => Directory.GetCurrentDirectory().EndsWith("ExpressionFramework")
+        => Directory.GetCurrentDirectory().EndsWith(ProjectName)
             ? System.IO.Path.Combine(Directory.GetCurrentDirectory(), @"src/")
             : System.IO.Path.Combine(Directory.GetCurrentDirectory(), @"../../../../");
 
     protected override string[] GetCustomBuilderTypes()
-        => GetCustomDefaultValueForBuilderClassConstructorValues().Select(x => x.Key.Split('.').Last()).ToArray();
+        => GetAbstractModels().Select(x => x.GetEntityClassName() ).ToArray();
 
     protected override Dictionary<string, string> GetCustomDefaultValueForBuilderClassConstructorValues()
         => new(GetAbstractModels().Select(x => new KeyValuePair<string, string>($"{RootNamespace}.{x.GetEntityClassName()}", "null")));
@@ -45,25 +45,25 @@ public abstract partial class ExpressionFrameworkCSharpClassBase : CSharpClassBa
     );
 
     protected override string[] GetNonDomainTypes()
-        => typeof(ExpressionFrameworkCSharpClassBase).Assembly.GetExportedTypes()
+        => GetType().Assembly.GetExportedTypes()
             .Where(x => x.IsInterface && x.Namespace == CodeGenerationRootNamespace)
             .Select(x => $"{RootNamespace}.{x.Name}")
             .ToArray();
 
     protected ITypeBase[] GetCoreModels()
         => MapCodeGenerationModelsToDomain(
-            typeof(ExpressionFrameworkCSharpClassBase).Assembly.GetExportedTypes()
+            GetType().Assembly.GetExportedTypes()
                 .Where(x => x.IsInterface && x.Namespace == $"{CodeGenerationRootNamespace}.Models" && !GetCustomBuilderTypes().Contains(x.GetEntityClassName())));
 
     protected ITypeBase[] GetAbstractModels()
         => MapCodeGenerationModelsToDomain(
-            typeof(ExpressionFrameworkCSharpClassBase).Assembly.GetExportedTypes()
+            GetType().Assembly.GetExportedTypes()
                 .Where(x => x.IsInterface && x.GetInterfaces().Length == 1)
                 .Select(x => x.GetInterfaces()[0])
                 .Distinct());
 
     protected ITypeBase[] GetOverrideModels(Type abstractType)
         => MapCodeGenerationModelsToDomain(
-            typeof(ExpressionFrameworkCSharpClassBase).Assembly.GetExportedTypes()
+            GetType().Assembly.GetExportedTypes()
                 .Where(x => x.IsInterface && x.GetInterfaces().FirstOrDefault() == abstractType));
 }
