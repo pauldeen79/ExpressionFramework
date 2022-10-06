f5a768a281b4fdf4809d81c0dbff60f13858f169
Paul Deen <p.deen@agrifirm.com>
2022-10-06 06:55:10 +0200

    Minor: Added nullable annotation to EnumerableExpression class, because the enumerable might contain null values

--- src/ExpressionFramework.Domain/EnumerableExpression.cs
+++ src/ExpressionFramework.Domain/EnumerableExpression.cs
@@ -2,9 +2,9 @@
 
 public static class EnumerableExpression
 {
-    public static Result<object?> GetResultFromEnumerable(IEnumerable e, Func<IEnumerable<object>, IEnumerable<Result<object?>>> @delegate)
+    public static Result<object?> GetResultFromEnumerable(IEnumerable e, Func<IEnumerable<object?>, IEnumerable<Result<object?>>> @delegate)
     {
-        var results = @delegate(e.OfType<object>()).TakeWhileWithFirstNonMatching(x => x.IsSuccessful()).ToArray();
+        var results = @delegate(e.OfType<object?>()).TakeWhileWithFirstNonMatching(x => x.IsSuccessful()).ToArray();
         if (!results.Last().IsSuccessful())
         {
             return results.Last();
@@ -13,9 +13,9 @@ public static class EnumerableExpression
         return Result<object?>.Success(results.Select(x => x.Value));
     }
 
-    public static Result<IEnumerable<object?>> GetTypedResultFromEnumerable(IEnumerable e, Func<IEnumerable<object>, IEnumerable<Result<object?>>> @delegate)
+    public static Result<IEnumerable<object?>> GetTypedResultFromEnumerable(IEnumerable e, Func<IEnumerable<object?>, IEnumerable<Result<object?>>> @delegate)
     {
-        var results = @delegate(e.OfType<object>()).TakeWhileWithFirstNonMatching(x => x.IsSuccessful()).ToArray();
+        var results = @delegate(e.OfType<object?>()).TakeWhileWithFirstNonMatching(x => x.IsSuccessful()).ToArray();
         if (!results.Last().IsSuccessful())
         {
             return Result<IEnumerable<object?>>.FromExistingResult(results.Last());
