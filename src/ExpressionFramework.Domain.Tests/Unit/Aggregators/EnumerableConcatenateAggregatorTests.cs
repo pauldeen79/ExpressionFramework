695c8d09fd88a29b961b0a6b5329d68a65792aad
Paul Deen <paul.deen@dutchworkz.nl>
2022-10-16 16:28:17 +0200

    Added EnumerableConcatenateAggregator

---
+++ src/ExpressionFramework.Domain.Tests/Unit/Aggregators/EnumerableConcatenateAggregatorTests.cs
@@ -0,0 +1,60 @@
+﻿namespace ExpressionFramework.Domain.Tests.Unit.Aggregators;
+
+public class EnumerableConcatenateAggregatorTests
+{
+    [Fact]
+    public void Aggregate_Returns_Invalid_When_Context_Is_Not_Enumerable()
+    {
+        // Arrange
+        var sut = new EnumerableConcatenateAggregator();
+
+        // Act
+        var result = sut.Aggregate(null, new ConstantExpression(new[] { "b" }));
+
+        // Assert
+        result.Status.Should().Be(ResultStatus.Invalid);
+        result.ErrorMessage.Should().Be("Context is not of type enumerable");
+    }
+
+    [Fact]
+    public void Aggregate_Returns_Error_When_SecondExpression_Returns_Error()
+    {
+        // Arrange
+        var sut = new EnumerableConcatenateAggregator();
+
+        // Act
+        var result = sut.Aggregate(new[] { "a" }, new ErrorExpression("Kaboom"));
+
+        // Assert
+        result.Status.Should().Be(ResultStatus.Error);
+        result.ErrorMessage.Should().Be("Kaboom");
+    }
+
+    [Fact]
+    public void Aggregate_Returns_Invalid_When_SecondExpression_Result_Value_Is_Not_Enumerable()
+    {
+        // Arrange
+        var sut = new EnumerableConcatenateAggregator();
+
+        // Act
+        var result = sut.Aggregate(new[] { "a" }, new ConstantExpression(1));
+
+        // Assert
+        result.Status.Should().Be(ResultStatus.Invalid);
+        result.ErrorMessage.Should().Be("Second expression is not of type enumerable");
+    }
+
+    [Fact]
+    public void Aggregate_Returns_Concatenated_Value_When_Context_And_SecondExpression_Result_Value_Are_String()
+    {
+        // Arrange
+        var sut = new EnumerableConcatenateAggregator();
+
+        // Act
+        var result = sut.Aggregate(new[] { "a" }, new ConstantExpression(new[] { "b" }));
+
+        // Assert
+        result.Status.Should().Be(ResultStatus.Ok);
+        result.Value.Should().BeEquivalentTo(new[] { "a", "b" });
+    }
+}
---
+++ src/ExpressionFramework.Domain/Aggregators/EnumerableConcatenateAggregator.cs
@@ -0,0 +1,26 @@
+﻿namespace ExpressionFramework.Domain.Aggregators;
+
+public partial record EnumerableConcatenateAggregator
+{
+    public override Result<object?> Aggregate(object? context, Expression secondExpression)
+    {
+        if (context is not IEnumerable e1)
+        {
+            return Result<object?>.Invalid("Context is not of type enumerable");
+        }
+
+        var secondExpressionResult = secondExpression.Evaluate(context);
+        if (!secondExpressionResult.IsSuccessful())
+        {
+            return secondExpressionResult;
+        }
+
+        if (secondExpressionResult.Value is not IEnumerable e2)
+        {
+            return Result<object?>.Invalid("Second expression is not of type enumerable");
+        }
+
+        return Result<object?>.Success(e1.OfType<object?>().Concat(e2.OfType<object?>()));
+    }
+}
+
