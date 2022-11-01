cd22a7377db547782d3d56b8ecb30381a54e59ed
Paul Deen <p.deen@agrifirm.com>
2022-11-01 09:40:55 +0100

    Refactored IfExpression, so that the public method is moved to SwitchExpression. It was only used from there, so it should just be part of that expression.

--- src/ExpressionFramework.Domain.Tests/Unit/Expressions/IfExpressionTests.cs
+++ src/ExpressionFramework.Domain.Tests/Unit/Expressions/IfExpressionTests.cs
@@ -17,36 +17,6 @@ public class IfExpressionTests
     }
 
     [Fact]
-    public void EvaluateWithConditionResult_Returns_Error_From_Expression_Evaluation()
-    {
-        // Arrange
-        var sut = new IfExpression(new SingleEvaluatable(new ErrorExpression(new ConstantExpression("Kaboom")), new EqualsOperator(), new EmptyExpression()), new EmptyExpression(), null);
-
-        // Act
-        var result = sut.EvaluateWithConditionResult(null);
-
-        // Assert
-        result.Status.Should().Be(ResultStatus.Error);
-        result.ErrorMessage.Should().Be("Kaboom");
-    }
-
-    [Fact]
-    public void EvaluateWithConditionResult_Returns_DefaultExpression_Result_When_Available_And_ConditionEvalution_Returns_False()
-    {
-        // Arrange
-        var sut = new IfExpression(new SingleEvaluatable(new ConstantExpression("Something"), new EqualsOperator(), new ConstantExpression("Something else")), new EmptyExpression(), new ConstantExpression("Default value"));
-
-        // Act
-        var result = sut.EvaluateWithConditionResult(null);
-
-        // Assert
-        result.Status.Should().Be(ResultStatus.Ok);
-        result.Value.ConditionResult.Should().BeFalse();
-        result.Value.ExpressionResult.Status.Should().Be(ResultStatus.Ok);
-        result.Value.ExpressionResult.Value.Should().Be("Default value");
-    }
-
-    [Fact]
     public void Can_Determine_Descriptor_Provider()
     {
         // Arrange
