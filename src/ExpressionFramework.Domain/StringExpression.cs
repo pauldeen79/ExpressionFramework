1878ab38ee20039ca1ec7f15fedcb183b13aeaed
Paul Deen <paul.deen@dutchworkz.nl>
2022-11-06 13:13:19 +0100

    Attempt to fix code duplication

--- src/ExpressionFramework.Domain/StringExpression.cs
+++ src/ExpressionFramework.Domain/StringExpression.cs
@@ -8,25 +8,17 @@ public static class StringExpression
         string expressionDescription,
         string okValue,
         string okDescription)
-        => new(
-            type.Name,
-            type.FullName,
+        => GetDescriptor(
+            type,
             description,
+            expressionDescription,
+            okValue, 
+            okDescription,
+            "LengthExpression",
+            "Number of characters to use",
+            typeof(int),
             true,
-            null,
-            null,
-            null,
-            new[]
-            {
-                new ParameterDescriptor("LengthExpression", typeof(int).FullName, "Number of characters to use", true),
-                new ParameterDescriptor("Expression", typeof(string).FullName, expressionDescription, true),
-            },
-            new[]
-            {
-                new ReturnValueDescriptor(ResultStatus.Ok, okValue, typeof(string), okDescription),
-                new ReturnValueDescriptor(ResultStatus.Invalid, "Empty", null, "Expression must be of type string, LengthExpression did not return an integer, Length must refer to a location within the string"),
-            }
-        );
+            "Expression must be of type string, LengthExpression did not return an integer, Length must refer to a location within the string");
 
     public static ExpressionDescriptor GetStringTrimDescriptor(
         Type type,
@@ -34,6 +26,31 @@ public static class StringExpression
         string expressionDescription,
         string okValue,
         string okDescription)
+        => GetDescriptor(
+            type,
+            description,
+            expressionDescription,
+            okValue,
+            okDescription,
+            "TrimCharsExpression",
+            "Optional trim characters to use. When empty, space will be used",
+            typeof(char[]),
+            false,
+            "Expression must be of type string, TrimCharsExpression must be of type char[]");
+
+#pragma warning disable S107 // Methods should not have too many parameters
+    private static ExpressionDescriptor GetDescriptor(
+        Type type,
+        string description,
+        string expressionDescription,
+        string okValue,
+        string okDescription,
+        string customExpressionName,
+        string customExpressionDescription,
+        Type customExpressionType,
+        bool customExpressionRequired,
+        string invalidDescription)
+#pragma warning restore S107 // Methods should not have too many parameters
         => new(
             type.Name,
             type.FullName,
@@ -44,13 +61,13 @@ public static class StringExpression
             null,
             new[]
             {
-                new ParameterDescriptor("TrimCharsExpression", typeof(int).FullName, "Optional trim characters to use. When empty, space will be used", true),
+                new ParameterDescriptor(customExpressionName, customExpressionType.FullName, customExpressionDescription, customExpressionRequired),
                 new ParameterDescriptor("Expression", typeof(string).FullName, expressionDescription, true),
             },
             new[]
             {
                 new ReturnValueDescriptor(ResultStatus.Ok, okValue, typeof(string), okDescription),
-                new ReturnValueDescriptor(ResultStatus.Invalid, "Empty", null, "Expression must be of type string, TrimCharsExpression must be of type char[]"),
+                new ReturnValueDescriptor(ResultStatus.Invalid, "Empty", null, invalidDescription),
             }
         );
 }
