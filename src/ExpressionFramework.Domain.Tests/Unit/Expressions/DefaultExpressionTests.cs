55d45092889fd1e3b47e8ef7cee5651e154dde94
Paul Deen <paul.deen@dutchworkz.nl>
2023-04-17 12:32:15 +0200

    Added missing unit test

--- src/ExpressionFramework.Domain.Tests/Unit/Expressions/DefaultExpressionTests.cs
+++ src/ExpressionFramework.Domain.Tests/Unit/Expressions/DefaultExpressionTests.cs
@@ -45,6 +45,16 @@ public class DefaultExpressionTests
     }
 
     [Fact]
+    public void BaseClass_Cannot_Evaluate()
+    {
+        // Arrange
+        var expression = new DefaultExpressionBase<int>();
+
+        // Act & Assert
+        expression.Invoking(x => x.Evaluate()).Should().Throw<NotImplementedException>();
+    }
+
+    [Fact]
     public void Can_Determine_Descriptor_Provider()
     {
         // Arrange
