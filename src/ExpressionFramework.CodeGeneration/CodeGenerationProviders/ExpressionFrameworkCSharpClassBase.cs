8bdc1f0d47100b6b4052d61701f5e4edfbd17f08
Paul Deen <paul.deen@dutchworkz.nl>
2023-04-17 16:27:52 +0200

    Upgraded ModelFramework.CodeGeneration, and added POC to using typed expressions

--- src/ExpressionFramework.CodeGeneration/CodeGenerationProviders/ExpressionFrameworkCSharpClassBase.cs
+++ src/ExpressionFramework.CodeGeneration/CodeGenerationProviders/ExpressionFrameworkCSharpClassBase.cs
@@ -16,4 +16,26 @@ public abstract partial class ExpressionFrameworkCSharpClassBase : CSharpClassBa
     protected override bool AddBackingFieldsForCollectionProperties => true;
     protected override bool AddPrivateSetters => true;
     protected override ArgumentValidationType ValidateArgumentsInConstructor => ArgumentValidationType.Shared;
+
+    protected override void FixImmutableBuilderProperty(ClassPropertyBuilder property, string typeName)
+    {
+        if (typeName.WithoutProcessedGenerics().GetClassName() == typeof(ITypedExpression<>).WithoutGenerics().GetClassName())
+        {
+            property.ConvertSinglePropertyToBuilderOnBuilder
+            (
+                $"{Constants.Namespaces.Domain}.Contracts.{typeof(ITypedExpression<>).WithoutGenerics().GetClassName()}Builder<{typeName.GetGenericArguments()}>",
+                property.IsNullable
+                    ? "_{1}Delegate = new (() => source.{0} == null ? null : {0}.ToBuilder())"
+                    : "_{1}Delegate = new (() => source.{0}.ToBuilder())"
+                );
+
+            if (!property.IsNullable)
+            {
+                // TODO: Find out if we want to assume you want to use a typed constant expression builder. It's polymorphic, so I guess the user has to decide?
+                /// property.SetDefaultValueForBuilderClassConstructor(new Literal($"new {Constants.Namespaces.DomainBuilders}.Expressions.TypedConstantExpressionBuilder<{typeName.GetGenericArguments()}>()"));
+            }
+        }
+
+        base.FixImmutableBuilderProperty(property, typeName);
+    }
 }
---
+++ src/ExpressionFramework.CodeGeneration/Models/Contracts/ITypedExpression.cs
@@ -0,0 +1,7 @@
+﻿namespace ExpressionFramework.CodeGeneration.Models.Contracts;
+
+#pragma warning disable S2326 // Unused type parameters should be removed
+public interface ITypedExpression<T>
+#pragma warning restore S2326 // Unused type parameters should be removed
+{
+}
