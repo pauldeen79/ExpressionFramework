0142aeda5ecf11624b71ba2e05f7e87ec8adf62f
Paul Deen <paul.deen@dutchworkz.nl>
2023-04-18 14:18:33 +0200

    Added missing unit tests

---
+++ src/ExpressionFramework.Domain.Tests/Unit/Builders/Expressions/LeftExpressionBuilderTests.cs
@@ -0,0 +1,17 @@
+﻿namespace ExpressionFramework.Domain.Tests.Unit.Builders.Expressions;
+
+public class LeftExpressionBuilderTests
+{
+    [Fact]
+    public void ITypedExpressionBuilder_Build_Returns_Correct_Result()
+    {
+        // Arrange
+        ITypedExpressionBuilder<string> sut = new LeftExpressionBuilder().WithExpression(new TypedConstantExpressionBuilder<string>().WithValue("Hello world"));
+
+        // Act
+        var result = sut.Build();
+
+        // Assert
+        result.Should().BeOfType<LeftExpression>();
+    }
+}
