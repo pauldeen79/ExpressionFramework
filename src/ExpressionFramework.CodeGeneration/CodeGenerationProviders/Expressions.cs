6486409eb607d1758101eb089e5b3aa1d3c1fe47
Paul Deen <paul.deen@dutchworkz.nl>
2023-04-23 08:09:19 +0200

    Refactored LINQ query

--- src/ExpressionFramework.CodeGeneration/CodeGenerationProviders/Expressions.cs
+++ src/ExpressionFramework.CodeGeneration/CodeGenerationProviders/Expressions.cs
@@ -11,10 +11,9 @@ public class Expressions : ExpressionFrameworkCSharpClassBase
 
     public override object CreateModel()
         => GetOverrideModels(typeof(IExpression))
-            .Select(x => new
+            .Select(x =>
             {
-                Source = x,
-                Target = new ClassBuilder()
+                var result = new ClassBuilder()
                 .WithNamespace(CurrentNamespace)
                 .WithName(x.Name)
                 .WithPartial()
@@ -25,24 +24,23 @@ public class Expressions : ExpressionFrameworkCSharpClassBase
                     .AddParameters(new ParameterBuilder().WithName("context").WithType(typeof(object)).WithIsNullable())
                     .WithTypeName($"{typeof(Result<>).WithoutGenerics()}<{typeof(object).FullName}?>")
                     .AddNotImplementedException()
-                )
-            })
-            .Select(x =>
-            {
-                var typedInterface = x.Source.Interfaces.FirstOrDefault(x => x != null && x.WithoutProcessedGenerics() == typeof(ITypedExpression<>).WithoutGenerics())?.FixTypeName();
+                );
+            
+                var typedInterface = x.Interfaces.FirstOrDefault(x => x != null && x.WithoutProcessedGenerics() == typeof(ITypedExpression<>).WithoutGenerics())?.FixTypeName();
                 if (!string.IsNullOrEmpty(typedInterface))
                 {
-                    x.Target.AddMethods(new ClassMethodBuilder()
-                            .WithName("EvaluateTyped")
-                            .AddParameters(new ParameterBuilder().WithName("context").WithType(typeof(object)).WithIsNullable())
-                            .WithTypeName($"{typeof(Result<>).WithoutGenerics()}<{typedInterface.GetGenericArguments()}>")
-                            .AddNotImplementedException()
+                    result
+                        .AddMethods(new ClassMethodBuilder()
+                        .WithName("EvaluateTyped")
+                        .AddParameters(new ParameterBuilder().WithName("context").WithType(typeof(object)).WithIsNullable())
+                        .WithTypeName($"{typeof(Result<>).WithoutGenerics()}<{typedInterface.GetGenericArguments()}>")
+                        .AddNotImplementedException()
                     );
                 }
 
-                return x.Target
-                    .AddGenericTypeArguments(x.Source.GenericTypeArguments)
-                    .AddGenericTypeArgumentConstraints(x.Source.GenericTypeArgumentConstraints)
+                return result
+                    .AddGenericTypeArguments(x.GenericTypeArguments)
+                    .AddGenericTypeArgumentConstraints(x.GenericTypeArgumentConstraints)
                     .Build();
             });
 }
