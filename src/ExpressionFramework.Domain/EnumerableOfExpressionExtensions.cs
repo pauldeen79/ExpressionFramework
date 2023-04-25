14e3b3a08c8523a1cf3a45b32c3652a47929b6e5
Paul Deen <paul.deen@dutchworkz.nl>
2023-04-25 17:17:33 +0200

    Removed unused code after last refactoring

--- src/ExpressionFramework.Domain/EnumerableOfExpressionExtensions.cs
+++ src/ExpressionFramework.Domain/EnumerableOfExpressionExtensions.cs
@@ -7,16 +7,4 @@ public static class EnumerableOfExpressionExtensions
             .Select(x => x.Evaluate(context))
             .TakeWhileWithFirstNonMatching(x => x.IsSuccessful())
             .ToArray();
-
-    public static Result<T>[] EvaluateTypedUntilFirstError<T>(this IEnumerable<Expression> expressions, object? context, string? errorMessage = null)
-        => expressions
-            .Select(x => x.EvaluateTyped<T>(context, errorMessage))
-            .TakeWhileWithFirstNonMatching(x => x.IsSuccessful())
-            .ToArray();
-
-    public static Result<T>[] EvaluateTypedUntilFirstError<T>(this IEnumerable<ITypedExpression<T>> expressions, object? context)
-        => expressions
-            .Select(x => x.EvaluateTyped(context))
-            .TakeWhileWithFirstNonMatching(x => x.IsSuccessful())
-            .ToArray();
 }
