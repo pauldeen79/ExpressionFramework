e91b0dccad3c46e131aca25658c041d3ac2c4433
Paul Deen <paul.deen@dutchworkz.nl>
2023-04-27 19:07:01 +0200

    Pushed conversion for argument types down to CrossCutting

--- src/ExpressionFramework.Parser/FunctionResultParsers/LeftExpressionParser.cs
+++ src/ExpressionFramework.Parser/FunctionResultParsers/LeftExpressionParser.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.Design.Serialization;
-
-namespace ExpressionFramework.Parser.FunctionResultParsers;
+﻿namespace ExpressionFramework.Parser.FunctionResultParsers;
 
 public class LeftExpressionParser : IFunctionResultParser
 {
@@ -18,46 +16,19 @@ public class LeftExpressionParser : IFunctionResultParser
             return Result<object?>.Continue();
         }
 
-        var expressionResult = functionParseResult.GetArgumentValue(0, nameof(LeftExpression.Expression), evaluator);
+        var expressionResult = functionParseResult.GetArgumentStringValue(0, nameof(LeftExpression.Expression), evaluator);
         if (!expressionResult.IsSuccessful())
         {
             return Result<object?>.FromExistingResult(expressionResult);
         }
-        var lengthResult = functionParseResult.GetArgumentValue(1, nameof(LeftExpression.LengthExpression), evaluator);
+
+        var lengthResult = functionParseResult.GetArgumentInt32Value(1, nameof(LeftExpression.LengthExpression), evaluator, _parser);
         if (!lengthResult.IsSuccessful())
         {
             return Result<object?>.FromExistingResult(lengthResult);
         }
 
-        var expressionValue = expressionResult.Value as string;
-        if (expressionValue is null)
-        {
-            return Result<object?>.Invalid("Expression is not of type string");
-        }
-
-        int length = 0;
-        if (lengthResult.Value is int i1)
-        {
-            length = i1;
-        }
-        else if (lengthResult.Value is string s)
-        {
-            var parseResult = _parser.Parse(s, functionParseResult.FormatProvider, functionParseResult.Context);
-            if (!parseResult.IsSuccessful())
-            {
-                return Result<object?>.Invalid("Expression is not of type integer");
-            }
-            else if (parseResult.Value is int i2)
-            {
-                length = i2;
-            }
-            else
-            {
-                return Result<object?>.Invalid("Expression is not of type integer");
-            }
-        }
-
-        return new LeftExpression(expressionValue, length).Evaluate(functionParseResult.Context);
+        return new LeftExpression(expressionResult.Value!, lengthResult.Value).Evaluate(functionParseResult.Context);
     }
 }
 
