89dd9db64edb6126169fd8ff595e9c981279460c
Paul Deen <paul.deen@dutchworkz.nl>
2023-04-29 10:56:32 +0200

    Refactored getting IEnumerable<Expression> from arguments, so it can be generated

--- src/ExpressionFramework.Parser/FunctionResultParsers/ChainedExpressionParser.cs
+++ src/ExpressionFramework.Parser/FunctionResultParsers/ChainedExpressionParser.cs
@@ -7,9 +7,8 @@ public class ChainedExpressionParser : ExpressionParserBase
         var expressions = GetArgumentValue<IEnumerable>(functionParseResult, 0, nameof(ChainedExpression.Expressions), evaluator).Value.Invoke(functionParseResult.Context);
 
         return Result<Expression>.Success(new ChainedExpression(
-            expressions.IsSuccessful()
-                ? expressions.Value!.OfType<object>().Select(x => new DelegateExpression(_ => x)).Cast<Expression>()
-                : new Expression[] { new DelegateResultExpression(_ => Result<object?>.FromExistingResult(expressions)) } ));
+            GetExpressionsArgumentValue(functionParseResult, 0, nameof(ChainedExpression.Expressions), evaluator)
+        ));
     }
 
     public ChainedExpressionParser(IExpressionParser parser) : base(parser, @"Chained")
