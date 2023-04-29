3d2940fda3ff761cc0420c7a52b094a14ca3fef5
Paul Deen <paul.deen@dutchworkz.nl>
2023-04-29 15:37:15 +0200

    Fixed aggregate expression parser

--- src/ExpressionFramework.Parser/ExpressionResultParsers/AggregateExpressionParser.cs
+++ src/ExpressionFramework.Parser/ExpressionResultParsers/AggregateExpressionParser.cs
@@ -4,8 +4,8 @@ public class AggregateExpressionParser : ExpressionParserBase
 {
     protected override Result<Expression> DoParse(FunctionParseResult functionParseResult, IFunctionParseResultEvaluator evaluator)
     {
-        var aggregator = GetArgumentValue<Aggregator>(functionParseResult, 1, nameof(AggregateExpression.Aggregator), evaluator);
-        var result = aggregator.Value.Invoke(functionParseResult.Context);
+        var aggregatorArgumentResult = GetArgumentValue<Aggregator>(functionParseResult, 0, nameof(AggregateExpression.Aggregator), evaluator);
+        var result = aggregatorArgumentResult.Value.Invoke(functionParseResult.Context);
         return result.IsSuccessful()
             ? Result<Expression>.Success(new AggregateExpression(GetExpressionsArgumentValue(functionParseResult, 0, nameof(AggregateExpression.Expressions), evaluator), result.Value!))
             : Result<Expression>.FromExistingResult(result);
