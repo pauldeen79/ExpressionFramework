c8c87764455f9592faf92af530509c2fe1bdd989
Paul Deen <paul.deen@dutchworkz.nl>
2023-04-29 20:03:11 +0200

    Refactored last expression parser code generation class

--- src/ExpressionFramework.CodeGeneration/CodeGenerationProviders/ExpressionParsersFullyGenerated.cs
+++ src/ExpressionFramework.CodeGeneration/CodeGenerationProviders/ExpressionParsersFullyGenerated.cs
@@ -10,25 +10,14 @@ public class ExpressionParsersFullyGenerated : ExpressionFrameworkCSharpClassBas
     public override object CreateModel()
         => GetOverrideModels(typeof(IExpression))
             .Where(IsSupportedExpressionForGeneratedParser)
-            .Select(x => new ClassBuilder()
-                .WithNamespace(CurrentNamespace)
-                .WithName($"{x.Name}Parser")
-                .WithBaseClass("ExpressionParserBase")
-                .AddConstructors(new ClassConstructorBuilder()
-                    .AddParameter("parser", typeof(IExpressionParser))
-                    .WithChainCall($"base(parser, {x.Name.ReplaceSuffix("Expression", string.Empty, StringComparison.InvariantCulture).CsharpFormat()})")
-                )
-                .AddMethods(new ClassMethodBuilder()
-                    .WithName("DoParse")
-                    .WithTypeName($"{typeof(Result<>).WithoutGenerics()}<{Constants.Namespaces.Domain}.Expression>")
-                    .AddParameter("functionParseResult", typeof(FunctionParseResult))
-                    .AddParameter("evaluator", typeof(IFunctionParseResultEvaluator))
-                    .WithProtected()
-                    .WithOverride()
-                    .AddLiteralCodeStatements($"return Result<{Constants.Namespaces.Domain}.Expression>.Success(new {Constants.Namespaces.DomainExpressions}.{x.Name}({CreateArguments(x)}));")
-                )
-            .Build()
-            );
+            .Select(x => CreateParserClass
+            (
+                x,
+                "Expression",
+                x.Name.ReplaceSuffix("Expression", string.Empty, StringComparison.InvariantCulture),
+                true,
+                m => m.AddLiteralCodeStatements($"return Result<{Constants.Namespaces.Domain}.Expression>.Success(new {Constants.Namespaces.DomainExpressions}.{x.Name}({CreateArguments(x)}));")
+            ).Build());
 
     private static string CreateArguments(ITypeBase x)
     {
@@ -41,45 +30,28 @@ public class ExpressionParsersFullyGenerated : ExpressionFrameworkCSharpClassBas
             {
                 builder.AppendLine(",").Append("    ");
             }
-            if (prop.TypeName.GetClassName() == "Expression")
+            if (prop.TypeName.GetClassName() == Constants.Types.Expression)
             {
                 builder.Append($"new TypedDelegateResultExpression<object>(_ => functionParseResult.GetArgumentValue({index}, {prop.Name.CsharpFormat()}, functionParseResult.Context, evaluator))");
             }
-            else if (prop.TypeName == "ExpressionFramework.Domain.Contracts.ITypedExpression<System.Collections.IEnumerable>")
+            else if (prop.TypeName == $"{Constants.Namespaces.DomainContracts}.{Constants.Types.ITypedExpression}<{typeof(IEnumerable).FullName}>")
             {
                 builder.Append($"GetTypedExpressionsArgumentValue(functionParseResult, {index}, {prop.Name.CsharpFormat()}, evaluator)");
             }
-            else if (prop.TypeName == "System.Collections.Generic.IReadOnlyCollection<ExpressionFramework.Domain.Expression>")
+            else if (prop.TypeName == $"{typeof(IReadOnlyCollection<>).WithoutGenerics()}<{Constants.Namespaces.Domain}.{Constants.Types.Expression}>")
             {
                 builder.Append($"GetExpressionsArgumentValue(functionParseResult, {index}, {prop.Name.CsharpFormat()}, evaluator)");
             }
-            else if (prop.TypeName.WithoutProcessedGenerics().GetClassName() == "ITypedExpression")
+            else if (prop.TypeName.WithoutProcessedGenerics().GetClassName() == Constants.Types.ITypedExpression)
             {
-                switch (prop.TypeName.GetGenericArguments())
+                var genericType = GetGenericType(prop.TypeName.GetGenericArguments());
+                if (string.IsNullOrEmpty(genericType.ClrType))
                 {
-                    case "System.Int32":
-                    case "int":
-                        builder.Append($"new TypedDelegateResultExpression<int>(_ =>functionParseResult.GetArgumentInt32Value({index}, {prop.Name.CsharpFormat()}, functionParseResult.Context, evaluator, Parser))");
-                        break;
-                    case "System.Int64":
-                    case "long":
-                        builder.Append($"new TypedDelegateResultExpression<long>(_ =>functionParseResult.GetArgumentInt64Value({index}, {prop.Name.CsharpFormat()}, functionParseResult.Context, evaluator, Parser))");
-                        break;
-                    case "System.Boolean":
-                    case "bool":
-                        builder.Append($"new TypedDelegateResultExpression<bool>(_ =>functionParseResult.GetArgumentBooleanValue({index}, {prop.Name.CsharpFormat()}, functionParseResult.Context, evaluator, Parser))");
-                        break;
-                    case "System.DateTime":
-                    case "DateTime":
-                        builder.Append($"new TypedDelegateResultExpression<System.DateTime>(_ =>functionParseResult.GetArgumentDateTimeValue({index}, {prop.Name.CsharpFormat()}, functionParseResult.Context, evaluator, Parser))");
-                        break;
-                    case "System.Decimal":
-                    case "decimal":
-                        builder.Append($"new TypedDelegateResultExpression<decimal>(_ =>functionParseResult.GetArgumentDecimalValue({index}, {prop.Name.CsharpFormat()}, functionParseResult.Context, evaluator, Parser))");
-                        break;
-                    default:
-                        builder.Append($"GetArgumentValue<{prop.TypeName.GetGenericArguments()}>(functionParseResult, {index}, {prop.Name.CsharpFormat()}, evaluator)");
-                        break;
+                    builder.Append($"GetArgumentValue<{prop.TypeName.GetGenericArguments()}>(functionParseResult, {index}, {prop.Name.CsharpFormat()}, evaluator)");
+                }
+                else
+                {
+                    builder.Append($"new TypedDelegateResultExpression<{genericType.ClrType}>(_ =>functionParseResult.GetArgument{genericType.MethodType}Value({index}, {prop.Name.CsharpFormat()}, functionParseResult.Context, evaluator, Parser))");
                 }
             }
             else
@@ -90,4 +62,15 @@ public class ExpressionParsersFullyGenerated : ExpressionFrameworkCSharpClassBas
         }
         return builder.ToString();
     }
+
+    private static (string ClrType, string MethodType) GetGenericType(string genericArguments)
+        => genericArguments switch
+        {
+            "System.Int32" or "int" => ("int", "Int32"),
+            "System.Int64" or "long" => ("long", "Int64"),
+            "System.Boolean" or "bool" => ("bool", "Boolean"),
+            "System.DateTime" or "DateTime" => ("System.DateTime", "DateTime"),
+            "System.Decimal" or "decimal" => ("decimal", "Decimal"),
+            _ => (string.Empty, string.Empty)
+        };
 }
