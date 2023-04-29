dccc50e2272b2384138713d31a256455b3abe098
Paul Deen <paul.deen@dutchworkz.nl>
2023-04-29 15:38:45 +0200

    Undo'ed last commit, it was wrong

--- src/ExpressionFramework.Parser/ExpressionResultParsers/AggregateExpressionParser.cs
+++ src/ExpressionFramework.Parser/ExpressionResultParsers/AggregateExpressionParser.cs
@@ -4,7 +4,7 @@ public class AggregateExpressionParser : ExpressionParserBase
 {
     protected override Result<Expression> DoParse(FunctionParseResult functionParseResult, IFunctionParseResultEvaluator evaluator)
     {
-        var aggregatorArgumentResult = GetArgumentValue<Aggregator>(functionParseResult, 0, nameof(AggregateExpression.Aggregator), evaluator);
+        var aggregatorArgumentResult = GetArgumentValue<Aggregator>(functionParseResult, 1, nameof(AggregateExpression.Aggregator), evaluator);
         var result = aggregatorArgumentResult.Value.Invoke(functionParseResult.Context);
         return result.IsSuccessful()
             ? Result<Expression>.Success(new AggregateExpression(GetExpressionsArgumentValue(functionParseResult, 0, nameof(AggregateExpression.Expressions), evaluator), result.Value!))
