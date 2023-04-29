0ada1b5d21ccde65e08085e4223bb65bb1fa3844
Paul Deen <paul.deen@dutchworkz.nl>
2023-04-29 15:54:02 +0200

    Implemented expression parser for EvaluatableExpression

--- src/ExpressionFramework.Parser/ExpressionResultParsers/EvaluatableExpressionParser.cs
+++ src/ExpressionFramework.Parser/ExpressionResultParsers/EvaluatableExpressionParser.cs
@@ -1,20 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿namespace ExpressionFramework.Parser.ExpressionResultParsers;
 
-namespace ExpressionFramework.Parser.ExpressionResultParsers
+public class EvaluatableExpressionParser : ExpressionParserBase
 {
-    public class EvaluatableExpressionParser : ExpressionParserBase
+    protected override Result<Expression> DoParse(FunctionParseResult functionParseResult, IFunctionParseResultEvaluator evaluator)
     {
-        protected override Result<Expression> DoParse(FunctionParseResult functionParseResult, IFunctionParseResultEvaluator evaluator)
-        {
-            throw new NotImplementedException();
-        }
+        var evaluatableArgumentResult = GetArgumentValue<Evaluatable>(functionParseResult, 1, nameof(EvaluatableExpression.Condition), evaluator);
+        var result = evaluatableArgumentResult.Value.Invoke(functionParseResult.Context);
+        return result.IsSuccessful()
+            ? Result<Expression>.Success(new EvaluatableExpression(
+                result.Value!,
+                new DelegateResultExpression(_ => functionParseResult.GetArgumentValue(0, @"Expression", functionParseResult.Context, evaluator))
+                ))
+            : Result<Expression>.FromExistingResult(result);
+    }
 
-        public EvaluatableExpressionParser(IExpressionParser parser) : base(parser, @"Evaluatable")
-        {
-        }
+    public EvaluatableExpressionParser(IExpressionParser parser) : base(parser, @"Evaluatable")
+    {
     }
 }
 
