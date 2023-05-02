4d08f6a52f975715ee9f929932196ec327123053
Paul Deen <paul.deen@dutchworkz.nl>
2023-05-02 15:57:59 +0200

    Added tests for ConstantEvaluatableParser

--- src/ExpressionFramework.Parser/EvaluatableResultParsers/ConstantEvaluatableParser.cs
+++ src/ExpressionFramework.Parser/EvaluatableResultParsers/ConstantEvaluatableParser.cs
@@ -2,16 +2,16 @@
 
 public class ConstantEvaluatableParser : EvaluatableParserBase
 {
+    public ConstantEvaluatableParser() : base(@"ConstantEvaluatable")
+    {
+    }
+
     protected override Result<Evaluatable> DoParse(FunctionParseResult functionParseResult, IFunctionParseResultEvaluator evaluator, IExpressionParser parser)
     {
         var valueResult = functionParseResult.GetArgumentBooleanValueResult(0, nameof(ConstantEvaluatable.Value), functionParseResult.Context, evaluator, parser);
         return valueResult.IsSuccessful()
             ? Result<Evaluatable>.Success(new ConstantEvaluatable(valueResult.Value!))
             : Result<Evaluatable>.FromExistingResult(valueResult);
-    }
-
-    public ConstantEvaluatableParser(IExpressionParser parser) : base(@"ConstantEvaluatable")
-    {
     }
 }
 
