87a51e1042c28558aabe07fcd22576785e8917d3
Paul Deen <paul.deen@dutchworkz.nl>
2023-05-03 11:59:11 +0200

    Added missing unit tests for EvaluatableExpressionParser

---
+++ src/ExpressionFramework.Parser.Tests/ExpressionResultParsers/EvaluatableExpressionParserTests.cs
@@ -0,0 +1,60 @@
+﻿namespace ExpressionFramework.Parser.Tests.ExpressionResultParsers;
+
+public sealed class EvaluatableExpressionParserTests : IDisposable
+{
+    private readonly ServiceProvider _provider;
+
+    public EvaluatableExpressionParserTests()
+    {
+        _provider = new ServiceCollection()
+            .AddParsers()
+            .AddExpressionParser()
+            .BuildServiceProvider();
+    }
+
+    [Fact]
+    public void Parse_Returns_Success_When_Arguments_Are_Valid()
+    {
+        // Arrange
+        var parser = new EvaluatableExpressionParser();
+        var contextValue = new SingleEvaluatable(new ContextExpression(), new EqualsOperator(), "some value");
+        var functionParseResult = new FunctionParseResult("Evaluatable", new FunctionParseResultArgument[]
+        {
+            new FunctionArgument(new FunctionParseResult("Context", Enumerable.Empty<FunctionParseResultArgument>(), CultureInfo.InvariantCulture, contextValue)),
+            new LiteralArgument("some value"),
+        }, CultureInfo.InvariantCulture, contextValue);
+
+        // Act
+        var result = parser.Parse(functionParseResult, _provider.GetRequiredService<IFunctionParseResultEvaluator>(), _provider.GetRequiredService<IExpressionParser>());
+
+        // Assert
+        result.Status.Should().Be(ResultStatus.Ok);
+        result.Value.Should().BeOfType<EvaluatableExpression>();
+        ((EvaluatableExpression)result.Value!).Expression.Should().BeOfType<ConstantResultExpression>();
+        ((ConstantResultExpression)((EvaluatableExpression)result.Value!).Expression).Value.Should().BeOfType<Result<object?>>();
+        ((Result<object?>)((ConstantResultExpression)((EvaluatableExpression)result.Value!).Expression).Value).Value.Should().BeEquivalentTo("some value");
+        ((EvaluatableExpression)result.Value!).Condition.Should().BeSameAs(contextValue);
+    }
+
+    [Fact]
+    public void Parse_Returns_Failure_When_Arguments_Are_Not_Valid()
+    {
+        // Arrange
+        var parser = new EvaluatableExpressionParser();
+        var contextValue = new SingleEvaluatable(new ContextExpression(), new EqualsOperator(), "some value");
+        var functionParseResult = new FunctionParseResult("Evaluatable", new FunctionParseResultArgument[]
+        {
+            new FunctionArgument(new FunctionParseResult("Unknown_Function", Enumerable.Empty<FunctionParseResultArgument>(), CultureInfo.InvariantCulture, contextValue)),
+            new LiteralArgument("some value"),
+        }, CultureInfo.InvariantCulture, contextValue);
+
+        // Act
+        var result = parser.Parse(functionParseResult, _provider.GetRequiredService<IFunctionParseResultEvaluator>(), _provider.GetRequiredService<IExpressionParser>());
+
+        // Assert
+        result.Status.Should().Be(ResultStatus.NotSupported);
+        result.ErrorMessage.Should().Be("Unknown function found: Unknown_Function");
+    }
+
+    public void Dispose() => _provider.Dispose();
+}
--- src/ExpressionFramework.Parser/ExpressionResultParsers/EvaluatableExpressionParser.cs
+++ src/ExpressionFramework.Parser/ExpressionResultParsers/EvaluatableExpressionParser.cs
@@ -4,13 +4,13 @@ public class EvaluatableExpressionParser : ExpressionParserBase
 {
     protected override Result<Expression> DoParse(FunctionParseResult functionParseResult, IFunctionParseResultEvaluator evaluator, IExpressionParser parser)
     {
-        var evaluatableResult = functionParseResult.GetArgumentValueResult<Evaluatable>(1, nameof(EvaluatableExpression.Condition), evaluator, parser)
+        var evaluatableResult = functionParseResult.GetArgumentValueResult<Evaluatable>(0, nameof(EvaluatableExpression.Condition), evaluator, parser)
             .EvaluateTyped(functionParseResult.Context);
 
         return evaluatableResult.IsSuccessful()
             ? Result<Expression>.Success(new EvaluatableExpression(
                 evaluatableResult.Value!,
-                new ConstantResultExpression(functionParseResult.GetArgumentValueResult(0, @"Expression", functionParseResult.Context, evaluator, parser))
+                new ConstantResultExpression(functionParseResult.GetArgumentValueResult(1, @"Expression", functionParseResult.Context, evaluator, parser))
                 ))
             : Result<Expression>.FromExistingResult(evaluatableResult);
     }
