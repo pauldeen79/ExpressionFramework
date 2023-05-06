3f23e0b253b35dfbd2307e2dc39821561b35e7f2
Paul Deen <paul.deen@dutchworkz.nl>
2023-05-06 07:58:52 +0200

    Fixed TODO by upgrading to the latest version of ModelFramework.CodeGeneration. Also included upgrade of CrossCutting.Common to prevent version conflicts

--- src/ExpressionFramework.CodeGeneration/CodeGenerationProviders/FunctionParseResultArguments/OverrideBuilders.cs
+++ src/ExpressionFramework.CodeGeneration/CodeGenerationProviders/FunctionParseResultArguments/OverrideBuilders.cs
@@ -13,13 +13,4 @@ public class OverrideBuilders : FunctionParseResultArgumentsBase
             new[] { typeof(LiteralArgument), typeof(FunctionArgument) },
             ProjectName,
             base.CurrentNamespace);
-
-    //TODO: Review if the implementation in the base class is logical, or should support both interfaces and classes. I had to remove $"I{typeBase.Name}" in this override to make the code generation work...
-    protected override bool IsMemberValid(IParentTypeContainer parent, ITypeBase typeBase)
-        => parent is not null
-        && typeBase is not null
-        && (string.IsNullOrEmpty(parent.ParentTypeFullName)
-            || parent.ParentTypeFullName.GetClassName() == typeBase.Name
-            || GetModelAbstractBaseTyped().Any(x => x == parent.ParentTypeFullName.GetClassName())
-            || (BaseClass is not null && typeBase.Name == BaseClass.Name));
 }
