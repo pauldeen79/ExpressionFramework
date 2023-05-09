ced981b6f10ab3acb77beb429330e31a75867f3c
Paul Deen <30937073+pauldeen79@users.noreply.github.com>
2023-05-09 12:00:12 +0200

    Added typed field expression (#21)
    Added typed field expression for convenience (might need Cast and TryCast soon...)

---
+++ src/ExpressionFramework.CodeGeneration/Models/Expressions/ITypedFieldExpression.cs
@@ -0,0 +1,9 @@
+﻿namespace ExpressionFramework.CodeGeneration.Models.Expressions;
+
+public interface ITypedFieldExpression<T> : IExpression, ITypedExpression<T>
+{
+    [Required]
+    IExpression Expression { get; }
+    [Required]
+    ITypedExpression<string> FieldNameExpression { get; }
+}
