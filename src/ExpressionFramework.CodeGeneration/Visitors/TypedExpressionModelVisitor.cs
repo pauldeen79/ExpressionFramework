866062f3d41d4ff508e839e3c27ad4e46e7bd588
Paul Deen <paul.deen@dutchworkz.nl>
2023-05-18 10:38:48 +0200

    Removed code duplication in new code

--- src/ExpressionFramework.CodeGeneration/Visitors/TypedExpressionModelVisitor.cs
+++ src/ExpressionFramework.CodeGeneration/Visitors/TypedExpressionModelVisitor.cs
@@ -3,30 +3,17 @@
 [ExcludeFromCodeCoverage]
 public class TypedExpressionModelVisitor : IVisitor
 {
-    public int Order => 30;
+    public int Order => 32;
 
     public void Visit<TBuilder, TEntity>(TypeBaseBuilder<TBuilder, TEntity> typeBaseBuilder, VisitorContext context)
         where TBuilder : TypeBaseBuilder<TBuilder, TEntity>
-        where TEntity : ITypeBase
-    {
-        if (typeBaseBuilder.Namespace.ToString() != Constants.Namespaces.DomainModelsExpressions)
-        {
-            return;
-        }
-
-        var buildTypedMethod = typeBaseBuilder.Methods.First(x => x.Name.ToString() == "ToTypedEntity");
-        if (!context.TypedInterfaceMap.TryGetValue(buildTypedMethod.TypeName.ToString().WithoutProcessedGenerics(), out var typedInterface))
-        {
-            return;
-        }
-
-        typeBaseBuilder.AddMethods
+        where TEntity : ITypeBase => BuilderVisitorBase.Visit
         (
-            new ClassMethodBuilder()
-                .WithName("ToEntity")
-                .WithTypeName($"{Constants.Namespaces.DomainContracts}.{typeof(ITypedExpression<>).WithoutGenerics().GetClassName()}<{typedInterface.GetGenericArguments()}>")
-                .AddLiteralCodeStatements("return ToTypedEntity();")
-                .WithExplicitInterfaceName($"{Constants.Namespaces.DomainContracts}.{typeof(ITypedExpression<>).WithoutGenerics().GetClassName()}Model<{typedInterface.GetGenericArguments()}>")
-        ).AddInterfaces($"{Constants.Namespaces.DomainContracts}.{typeof(ITypedExpression<>).WithoutGenerics().GetClassName()}Model<{typedInterface.GetGenericArguments()}>");
-    }
+            typeBaseBuilder,
+            context,
+            Constants.Namespaces.DomainModelsExpressions,
+            "ToTypedEntity",
+            "ToEntity",
+            "Model"
+        );
 }
