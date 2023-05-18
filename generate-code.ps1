d37c9fe5c244775f0685ae6d95eb23512cfe068a
Paul Deen <paul.deen@dutchworkz.nl>
2023-05-18 15:38:48 +0200

    Fixed typo in script

--- generate-code.ps1
+++ generate-code.ps1
@@ -1,4 +1,4 @@
-# dotnet run --project src/QueryFramework.CodeGeneration\QueryFramework.CodeGeneration.csproj
+# dotnet run --project src/ExpressionFramework.CodeGeneration/ExpressionFramework.CodeGeneration.csproj
 
 dotnet build -c Debug src/ExpressionFramework.CodeGeneration/ExpressionFramework.CodeGeneration.csproj
 Invoke-Expression "t4plus assembly -a $(Resolve-Path "src/ExpressionFramework.CodeGeneration/bin/Debug/net7.0/ExpressionFramework.CodeGeneration.dll") -p $(Resolve-Path "src") -u $(Resolve-Path "src/ExpressionFramework.CodeGeneration/bin/Debug/net7.0")"
