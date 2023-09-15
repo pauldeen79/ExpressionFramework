5ebb92403ffc3d96fc228fe530b3800039ff707e
Paul Deen <paul.deen@dutchworkz.nl>
2023-09-15 08:43:08 +0200

    Minor: Removed unit test code duplication by using properties and constructor initialization

--- src/ExpressionFramework.Parser.Tests/Extensions/ExpressionFrameworkParserExtensionsTests.cs
+++ src/ExpressionFramework.Parser.Tests/Extensions/ExpressionFrameworkParserExtensionsTests.cs
@@ -2,19 +2,26 @@
 
 public class ExpressionFrameworkParserExtensionsTests : TestBase
 {
+    private IFunctionParseResultEvaluator Evaluator { get; }
+    private IExpressionParser ExpressionParser { get; }
+
+    public ExpressionFrameworkParserExtensionsTests()
+    {
+        Evaluator = Fixture.Freeze<IFunctionParseResultEvaluator>();
+        ExpressionParser = Fixture.Freeze<IExpressionParser>();
+    }
+
     [Fact]
     public void Parse_Returns_Failure_When_Source_Parsing_Fails()
     {
         // Arrange
-        var evaluator = Fixture.Freeze<IFunctionParseResultEvaluator>();
-        var expressionParser = Fixture.Freeze<IExpressionParser>();
         var sut = Fixture.Create<IExpressionFrameworkParser>();
         var functionParseResult = new FunctionParseResultBuilder().WithFunctionName("MyFunction").Build();
         sut.Parse(Arg.Any<FunctionParseResult>(), Arg.Any<IFunctionParseResultEvaluator>(), Arg.Any<IExpressionParser>())
            .Returns(Result<Expression>.Error("Kaboom"));
 
         // Act
-        var result = sut.Parse<string>(functionParseResult, evaluator, expressionParser);
+        var result = sut.Parse<string>(functionParseResult, Evaluator, ExpressionParser);
 
         // Assert
         result.Status.Should().Be(ResultStatus.Error);
@@ -25,15 +32,13 @@ public class ExpressionFrameworkParserExtensionsTests : TestBase
     public void Parse_Returns_Success_When_Source_Parsing_Succeeds_And_Result_Value_Is_Of_Correct_Type()
     {
         // Arrange
-        var evaluator = Fixture.Freeze<IFunctionParseResultEvaluator>();
-        var expressionParser = Fixture.Freeze<IExpressionParser>();
         var sut = Fixture.Create<IExpressionFrameworkParser>();
         var functionParseResult = new FunctionParseResultBuilder().WithFunctionName("MyFunction").Build();
         sut.Parse(Arg.Any<FunctionParseResult>(), Arg.Any<IFunctionParseResultEvaluator>(), Arg.Any<IExpressionParser>())
            .Returns(Result<Expression>.Success(new TypedConstantExpression<string>("test")));
 
         // Act
-        var result = sut.Parse<string>(functionParseResult, evaluator, expressionParser);
+        var result = sut.Parse<string>(functionParseResult, Evaluator, ExpressionParser);
 
         // Assert
         result.Status.Should().Be(ResultStatus.Ok);
@@ -44,15 +49,13 @@ public class ExpressionFrameworkParserExtensionsTests : TestBase
     public void Parse_Returns_Invalid_When_Source_Parsing_Succeeds_And_Result_Value_Is_Not_Of_Correct_Type()
     {
         // Arrange
-        var evaluator = Fixture.Freeze<IFunctionParseResultEvaluator>();
-        var expressionParser = Fixture.Freeze<IExpressionParser>();
         var sut = Fixture.Create<IExpressionFrameworkParser>();
         var functionParseResult = new FunctionParseResultBuilder().WithFunctionName("MyFunction").Build();
         sut.Parse(Arg.Any<FunctionParseResult>(), Arg.Any<IFunctionParseResultEvaluator>(), Arg.Any<IExpressionParser>())
            .Returns(Result<Expression>.Success(new TypedConstantExpression<int>(1)));
 
         // Act
-        var result = sut.Parse<string>(functionParseResult, evaluator, expressionParser);
+        var result = sut.Parse<string>(functionParseResult, Evaluator, ExpressionParser);
 
         // Assert
         result.Status.Should().Be(ResultStatus.Invalid);
