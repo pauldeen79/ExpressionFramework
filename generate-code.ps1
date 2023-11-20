68a39a7a6161d19cb11ffe559fa5babe93bd0bbe
Paul Deen <paul.deen@dutchworkz.nl>
2023-11-20 09:35:38 +0100

    Upgrade CodeGeneration project to .net 8.0

--- generate-code.ps1
+++ generate-code.ps1
@@ -1,4 +1,4 @@
 # dotnet run --project src/ExpressionFramework.CodeGeneration/ExpressionFramework.CodeGeneration.csproj
 
 dotnet build -c Debug src/ExpressionFramework.CodeGeneration/ExpressionFramework.CodeGeneration.csproj
-t4plus assembly -a src/ExpressionFramework.CodeGeneration/bin/Debug/net7.0/ExpressionFramework.CodeGeneration.dll -p src/
+t4plus assembly -a src/ExpressionFramework.CodeGeneration/bin/Debug/net8.0/ExpressionFramework.CodeGeneration.dll -p src/
