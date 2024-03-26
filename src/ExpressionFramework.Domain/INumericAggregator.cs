f18f6935daa505d4744587c2e62727e812f97dff
Paul Deen <paul.deen@dutchworkz.nl>
2024-03-26 13:25:00 +0100

    Minor: Removed unused code

--- src/ExpressionFramework.Domain/INumericAggregator.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ExpressionFramework.Domain;
-
-public interface INumericAggregator<T>
-{
-    Result<object?> Aggregate(object? context, Expression firstExpression, Expression secondExpression, Func<T, T, object> aggregatorDelegate);
-}
