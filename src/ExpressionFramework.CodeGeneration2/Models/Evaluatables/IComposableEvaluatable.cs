1c43ef8b6b71ee5b555f1f51d65f24d5def91516
Paul Deen <paul.deen@dutchworkz.nl>
2024-04-01 11:44:53 +0200

    Changed StartGroup and EndGroup from nullable to not nullable. Don't know why I did this?

--- src/ExpressionFramework.CodeGeneration2/Models/Evaluatables/IComposableEvaluatable.cs
+++ src/ExpressionFramework.CodeGeneration2/Models/Evaluatables/IComposableEvaluatable.cs
@@ -7,6 +7,6 @@ public interface IComposableEvaluatable : IEvaluatable
     [Required][ValidateObject] IExpression RightExpression { get; }
 
     Combination? Combination { get; }
-    bool? StartGroup { get; }
-    bool? EndGroup { get; }
+    bool StartGroup { get; }
+    bool EndGroup { get; }
 }
