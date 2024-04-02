38f9d1969da49d402b8b7ff7cda20707816a5754
Paul Deen <paul.deen@dutchworkz.nl>
2024-04-02 14:49:16 +0200

    Removed POC code

--- src/ExpressionFramework.Domain2/Builders/Expressions/TypedConstantExpressionBuilder.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ExpressionFramework.Domain.Builders.Expressions;
-
-public partial class TypedConstantExpressionBuilder<T>
-{
-    public static implicit operator TypedConstantExpressionBuilder<T>(T value) => new TypedConstantExpressionBuilder<T>().WithValue(value);
-}
--- src/ExpressionFramework.Domain2/Builders/Expressions/TypedDelegateExpressionBuilder.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ExpressionFramework.Domain.Builders.Expressions;
-
-public partial class TypedDelegateExpressionBuilder<T>
-{
-    public static implicit operator TypedDelegateExpressionBuilder<T>(Func<object?, T> value) => new TypedDelegateExpressionBuilder<T>().WithValue(value);
-}
