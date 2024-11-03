39ab0d497ca99aa114047c9572f977e4c3c203f1
Paul Deen <paul.deen@dutchworkz.nl>
2024-11-03 08:36:47 +0100

    Added explanation in code comment

--- src/ExpressionFramework.CodeGeneration/Models/Expressions/IStringFindExpression.cs
+++ src/ExpressionFramework.CodeGeneration/Models/Expressions/IStringFindExpression.cs
@@ -1,6 +1,8 @@
 ﻿namespace ExpressionFramework.CodeGeneration.Models.Expressions;
 
+//Example how to customize the expression name, with a namespace:
 //[ExpressionName("Find", "String")]
+//Example how to customize the expression name, without a namespace:
 //[ExpressionName("StringFind")]
 public interface IStringFindExpression : IExpression, ITypedExpression<int>
 {
