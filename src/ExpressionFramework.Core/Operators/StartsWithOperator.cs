1695a63e58d406b794a48c44d7a78f9712df5daf
Paul Deen <paul.deen@dutchworkz.nl>
2025-03-17 20:31:26 +0100

    Implemented last three operators

--- src/ExpressionFramework.Core/Operators/StartsWithOperator.cs
+++ src/ExpressionFramework.Core/Operators/StartsWithOperator.cs
@@ -1,12 +1,9 @@
-﻿namespace ExpressionFramework.Core.Operators
+﻿namespace ExpressionFramework.Core.Operators;
+
+public partial record StartsWithOperator
 {
-#nullable enable
-    public partial record StartsWithOperator
-    {
-        public override Result<bool> Evaluate(object? leftValue, object? rightValue, StringComparison stringComparison)
-        {
-            throw new NotImplementedException();
-        }
-    }
-#nullable restore
+    public override Result<bool> Evaluate(object? leftValue, object? rightValue, StringComparison stringComparison)
+        => leftValue is string leftString && rightValue is string rightString
+            ? Result.Success(leftString.StartsWith(rightString, StringComparison.CurrentCultureIgnoreCase))
+            : Result.Invalid<bool>("LeftValue and RightValue both need to be of type string");
 }
--- src/ExpressionFramework.Core/Operators/StringNotContainsOperator.cs
+++ src/ExpressionFramework.Core/Operators/StringNotContainsOperator.cs
@@ -1,12 +1,9 @@
-﻿namespace ExpressionFramework.Core.Operators
+﻿namespace ExpressionFramework.Core.Operators;
+
+public partial record StringNotContainsOperator
 {
-#nullable enable
-    public partial record StringNotContainsOperator
-    {
-        public override Result<bool> Evaluate(object? leftValue, object? rightValue, StringComparison stringComparison)
-        {
-            throw new NotImplementedException();
-        }
-    }
-#nullable restore
+    public override Result<bool> Evaluate(object? leftValue, object? rightValue, StringComparison stringComparison)
+        => leftValue is string leftString && rightValue is string rightString
+            ? Result.Success(leftString.IndexOf(rightString, StringComparison.CurrentCultureIgnoreCase) == -1)
+            : Result.Invalid<bool>("LeftValue and RightValue both need to be of type string");
 }
