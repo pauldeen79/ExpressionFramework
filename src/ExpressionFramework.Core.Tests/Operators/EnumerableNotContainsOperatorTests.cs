bb43d35b65549114883f1d6ff44ecc0225feb6f0
Paul Deen <paul.deen@dutchworkz.nl>
2025-03-17 16:20:48 +0100

    Implemented EnumerableNotContainsOperator

---
+++ src/ExpressionFramework.Core.Tests/Operators/EnumerableNotContainsOperatorTests.cs
@@ -0,0 +1,35 @@
+﻿namespace ExpressionFramework.Core.Tests.Operators;
+
+public class EnumerableNotContainsOperatorTests : TestBase<EnumerableNotContainsOperator>
+{
+    public class Evaluate : EnumerableNotContainsOperatorTests
+    {
+        [Fact]
+        public void Returns_Correct_Resul_When_LeftValue_Is_Enumerable()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act
+            var result = sut.Evaluate(new[] { 1, 2, 3 }, 5, StringComparison.Ordinal);
+
+            // Assert
+            result.Status.ShouldBe(ResultStatus.Ok);
+            result.Value.ShouldBe(true);
+        }
+
+        [Fact]
+        public void Returns_Invalid_When_LeftValue_Is_Not_Enumerable()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act
+            var result = sut.Evaluate(false, 5, StringComparison.Ordinal);
+
+            // Assert
+            result.Status.ShouldBe(ResultStatus.Invalid);
+            result.ErrorMessage.ShouldBe("Left value is not of type IEnumerable");
+        }
+    }
+}
--- src/ExpressionFramework.Core/Operators/EnumerableNotContainsOperator.cs
+++ src/ExpressionFramework.Core/Operators/EnumerableNotContainsOperator.cs
@@ -1,12 +1,9 @@
-﻿namespace ExpressionFramework.Core.Operators
+﻿namespace ExpressionFramework.Core.Operators;
+
+public partial record EnumerableNotContainsOperator
 {
-#nullable enable
-    public partial record EnumerableNotContainsOperator
-    {
-        public override Result<bool> Evaluate(object? leftValue, object? rightValue, StringComparison stringComparison)
-        {
-            throw new NotImplementedException();
-        }
-    }
-#nullable restore
+    public override Result<bool> Evaluate(object? leftValue, object? rightValue, StringComparison stringComparison)
+        => leftValue is IEnumerable enumerableLeft
+            ? Result.Success(!enumerableLeft.OfType<object>().Contains(rightValue))
+            : Result.Invalid<bool>("Left value is not of type IEnumerable");
 }
